6d1602bfb619cc4ace873dd6f492f4038678029b
goose9991 <150225037+goose9991@users.noreply.github.com>
2025-01-28 16:32:49 +0000

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,13 @@
-
+public class BankAccount {
+    public static void main(String[] args){
+        double balance = 2175.37;
+        balance -= 302.5;
+        balance += 50.03;
+        balance /= 2;
+        balance += 20;
+        balance -= 1;
+        balance *= 2;
+        balance += 1;
+        System.out.printf("%.2f\n", balance);
+    }
+}
