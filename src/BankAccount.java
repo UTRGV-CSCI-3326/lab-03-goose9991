f6699d8aba8b927be43feef4431298372be2c2e0
goose9991 <150225037+goose9991@users.noreply.github.com>
2025-01-30 08:08:40 -0600

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,8 @@
 public class BankAccount {
     public static void main(String[] args){
+        //initialize starting balance
         double balance = 2175.37;
+        //perform transactions
         balance -= 302.5;
         balance += 50.03;
         balance /= 2;
@@ -8,6 +10,7 @@ public class BankAccount {
         balance -= 1;
         balance *= 2;
         balance += 1;
+        //print final balance formatted to hundredths place
         System.out.printf("%.2f\n", balance);
     }
 }
